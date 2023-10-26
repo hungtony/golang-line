1472be5b0442f5437d66bc2a10069b6829c3338c
TONY <tmoanryk@gmail.com>
2023-10-26 20:58:17 +0800

    commit #4 Create a Go package connect to mongoDB, create a model/DTO to save/query user message to MongoDB

---
+++ model/mongo.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoRepository struct {
+	client *mongo.Client
+}
+
+func NewMongoRepository(client *mongo.Client) *MongoRepository {
+	return &MongoRepository{
+		client: client,
+	}
+}
+
+func (r *MongoRepository) SaveMessage(ctx context.Context, message *Message) error {
+	collection := r.client.Database("local").Collection("startup_log")
+
+	_, err := collection.InsertOne(ctx, message)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *MongoRepository) GetMessagesByUserID(ctx context.Context, userID string) ([]*Message, error) {
+	collection := r.client.Database("line_bot").Collection("messages")
+
+	cursor, err := collection.Find(ctx, bson.M{"user_id": userID})
+	if err != nil {
+		return nil, err
+	}
+
+	defer cursor.Close(ctx)
+
+	var messages []*Message
+	for cursor.Next(ctx) {
+		message := &Message{}
+		err := cursor.Decode(message)
+		if err != nil {
+			return nil, err
+		}
+
+		messages = append(messages, message)
+	}
+
+	return messages, nil
+}
